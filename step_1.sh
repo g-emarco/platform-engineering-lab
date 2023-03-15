8100b2ea32ac559e9f9e4fc632740455e104352e
emarco <emarco@google.com>
2023-03-15 14:16:32 +0200

    fixed paths

--- step_1.sh
+++ step_1.sh
@@ -1,6 +1,5 @@
-git clone -b 1-start-here https://github.com/g-emarco/platform-engineering-lab.git
-cd platform-engineering-lab/
 cd infra/
 terraform init
 terraform apply -auto-approve
 echo "NOW LETS CREATE FIRETSTORE DB in EUR"
+cd ..
--- step_2.sh
+++ step_2.sh
@@ -1,3 +1,3 @@
-cd platform-engineering-lab
 cd platform_api
 gcloud run deploy platform-api --region=europe-west3 --set-env-vars=FLASK_APP=main.py --service-account=bucket-accessor-sa@${TF_VAR_project_id}.iam.gserviceaccount.com --no-allow-unauthenticated --source .
+cd ..
